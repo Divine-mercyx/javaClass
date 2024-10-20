f73c12441ac4d9f8870fe7d06b1b707272498f42
Divine Obinali <divinewilson766@gmail.com>
2024-10-20 08:44:29 +0100

    Update Power.java

--- mrChibuzor/Power.java
+++ mrChibuzor/Power.java
@@ -2,7 +2,7 @@ public class Power {
 	public static void main(String[] args) {
 		int first = 2;
 		int second = 3;
-		int firstPower = first * first * first;
+		int firstPower = Math.pow(first, second);
 		System.out.println(firstPower);
 	}
 }
