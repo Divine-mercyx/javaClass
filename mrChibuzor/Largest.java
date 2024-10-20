f10ac44e241f9203db5da7e9ead8b0e0e1b0c7fa
Divine Obinali <divinewilson766@gmail.com>
2024-10-20 09:39:50 +0100

    added Largest.java to repo

---
+++ mrChibuzor/Largest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Largest {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		boolean condition = true;
+		double largest = 0;
+
+
+		while (condition) {
+			System.out.print("enter a number: ");
+			int number = input.nextInt();
+			
+			if (number > largest) {
+				largest = number;
+			}
+			System.out.print("\ndo you want to continue 1 for(yes) / 2 for(no)");
+			int question = input.nextInt();
+			if (question == 1) {
+				continue;
+			}
+			else if (question == 2) {
+				break;
+			} else {
+				System.out.println("\ninvalid choice!");
+			}
+		}
+		System.out.printf("the largest number is: %.0f", largest);
+	}
+}
