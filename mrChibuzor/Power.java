bee115139a3cec9418484e0f21a4dc3f3e236367
Divine Obinali <divinewilson766@gmail.com>
2024-10-20 09:21:00 +0100

    Update Power.java

--- mrChibuzor/Power.java
+++ mrChibuzor/Power.java
@@ -1,13 +1,8 @@
-import java.util.Scanner;
-
 public class Power {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("enter first number: ");
-		int first = input.nextInt();
-		System.out.print("enter second number: ");
-		int second = input.nextInt();
-		int firstPower = Math.pow(first, second);
+		int first = 2
+		int second = 3
+		int firstPower = first * first * first;
 		System.out.println(firstPower);
 	}
 }
