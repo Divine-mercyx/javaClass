8d02c127742b398697d98c8a336d3695f1224b44
Divine Obinali <divinewilson766@gmail.com>
2024-12-03 11:52:34 +0100

    adding student grade

---
+++ mrChibuzor/phaseGateOne/StudentGrade.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class StudentGrade {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StudentGrade studentGrade = new StudentGrade();
+        
+        System.out.print("enter how many students: ");
+        int student_number = input.nextInt();
+        System.out.print("enter how many subjects: ");
+        int subject_number = input.nextInt();
+        int[] numbers = studentGrade.getNumberOfStudentsAndSubjects(student_number, subject_number);
+        int[][] students = studentGrade.collectScoresForEveryStudentAndSubject(numbers);
+        int[][] results = studentGrade.askQuestionsForStudents(students, input);
+        studentGrade.displaySummary(results);
+        
+        
+        
+    }
+
+    public int[] getNumberOfStudentsAndSubjects(int numberOfStudents, int numberOfSubjects) {
+        int[] numbers = {numberOfStudents, numberOfSubjects};
+        return numbers;
+    }
+    
+    public int[][] collectScoresForEveryStudentAndSubject(int[] numbers) {
+        int student = numbers[0];
+        int subject = numbers[1];
+        int[][] students = new int[student][subject];
+        return students;
+    }
+    
+    public int[][] askQuestionsForStudents(int[][] numbers, Scanner input) {
+        for (int row = 0; row < numbers.length; row++) {
+            System.out.println();
+            System.out.println("entering score for student " + (row + 1));
+            for (int column = 0; column < numbers[row].length; column++) {
+                System.out.print("entering score for subject " + (column + 1) + ": ");
+                int answer = input.nextInt();
+                if (answer >= 0 && answer <= 100) {
+                    numbers[row][column] = answer;
+                } else {
+                    System.out.println("error, invalid input: number must be zero or above zero. Try again...");
+                    column--;
+                }
+            }
+        }
+        return numbers;
+    }
+    
+    
+    public  void displaySummary(int[][] numbers) {
+        
+        int position = numbers.length;
+        System.out.println("=============================================================================");
+        displayHeader(numbers);
+        System.out.println("=============================================================================");
+        for (int row = 0; row < numbers.length; row++) {
+            int total = 0;
+            System.out.print("Student " + (row + 1) + "\t");
+            for (int column = 0; column < numbers[row].length; column++) {
+                System.out.print(numbers[row][column] + "\t");
+                total += numbers[row][column];
+            }
+            int lengths = numbers[row].length;
+            double average = total / lengths;
+            System.out.println(total + "\t" + average + "\t  " + (position--));
+        }
+        System.out.println("=============================================================================");
+        System.out.println("=============================================================================");
+        subjectSummary(numbers);
+    }
+    
+    public void displayHeader(int[][] numbers) {
+        System.out.print("Students\t");
+        for (int row = 0; row < numbers.length; row++) {
+            for (int column = 0; column < numbers[row].length; column++) {
+                System.out.print("sub " + (column + 1) + "\t");
+            }
+            System.out.println("TOT\tAVE\tPOS");
+            break;
+        }
+    }
+    
+    public void subjectSummary(int[][] numbers) {
+        System.out.println("Subject summary");
+        for (int row = 0; row < numbers[0].length; row++) {
+            decideForEachSubject(row, numbers);
+        }
+    }
+    
+    public void decideForEachSubject(int row, int[][] numbers) {
+        int largest = 0;
+        int largestIndex = 0;
+        int smallest = 0;
+        int smallestIndex = 0;
+        int total = 0;
+        for (int column = 0; column < numbers.length; column++) {
+            if (numbers[column][row] > largest) {
+                largest = numbers[column][row];
+                largestIndex = column;
+            }
+            else if (numbers[column][row] < smallest) {
+                numbers[column][row] = smallest;
+                smallestIndex = column;
+            }
+            total += numbers[column][row];
+        }
+        System.out.println("Subject " + (row + 1));
+        System.out.println("highest scoring is:       student " + (largestIndex + 1) + " scoring: " + largest);
+        System.out.println("lowest scoring is:       student " + (smallestIndex + 1) + " scoring: " + smallest + "\nTotal score: " + total);
+        System.out.println();
+    }
+    
+    
+}
---
+++ mrChibuzor/phaseGateOne/StudentGradeTest.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StudentGradeTest {
+
+    @Test
+    public void getReturnedStudentsNumberAndSubjectNumber() {
+        StudentGrade studentGrade = new StudentGrade();
+        int studentNumber = 5;
+        int subjectNumber = 5;
+        int[] actual = studentGrade.getNumberOfStudentsAndSubjects(studentNumber, subjectNumber);
+        int[] expected = {5, 5};
+        assertArrayEquals(actual, expected);
+    }
+    
+    @Test
+    public void checkIfcollectScoresForEveryStudentAndSubjectReturns2DArray() {
+        StudentGrade studentGrade = new StudentGrade();
+        int[] numbers = {3, 3};
+        int[][] actual = studentGrade.collectScoresForEveryStudentAndSubject(numbers);
+        int[][] expected = new int[3][3];
+        assertArrayEquals(actual, expected);
+    }
+}
