49af1ba3cf5820b91e0002f1bbb874545097d9fe
Divine Obinali <divinewilson766@gmail.com>
2024-12-04 14:56:29 +0100

    finished phonebookapp fro java

--- mrChibuzor/phaseGateOne/PhonebookApp.java
+++ mrChibuzor/phaseGateOne/PhonebookApp.java
@@ -7,14 +7,16 @@ public class PhonebookApp {
         Scanner input = new Scanner(System.in);
         PhonebookApp person = new PhonebookApp();
         ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> last = new ArrayList<>();
         ArrayList<String> addresses = new ArrayList<>();
-        ArrayList<Long> numbers = new ArrayList<>();
+        ArrayList<String> numbers = new ArrayList<>();
         ArrayList<String> emails = new ArrayList<>();
         
-        person.getQuestionsAndOptions(names, addresses, numbers, emails);
+        person.getQuestionsAndOptions(names, last, addresses, numbers, emails);
     }
     
     public String displayOptions() {
+        System.out.println();
         String options = """
         1. Add Contact
         2. Remove Contact
@@ -28,75 +30,154 @@ public class PhonebookApp {
     }
     
 
-    public void getQuestionsAndOptions(ArrayList<String> names, ArrayList<String> addresses, ArrayList<Long> numbers, ArrayList<String> emails) {
+    public void getQuestionsAndOptions(ArrayList<String> names, ArrayList<String> last, ArrayList<String> addresses, ArrayList<String> numbers, ArrayList<String> emails) {
         Scanner input = new Scanner(System.in);
         String menu = displayOptions();
         System.out.println(menu);
         int options = input.nextInt();
         switch (options) {
-            case 1: askContactsInfo(names, addresses, numbers, emails); break;
-            case 2: removeContact(names, addresses, numbers, emails); break;
-            case 3: break;
-            case 4: break;
-            case 5: break;
-            case 6: break;
+            case 1: askContactsInfo(names, last, addresses, numbers, emails); break;
+            case 2: removeContact(names, last, addresses, numbers, emails); break;
+            case 3: findContact(names, last, addresses, numbers, emails); break;
+            case 4: findContactFirstName(names, last, addresses, numbers, emails); break;
+            case 5: findContactLastName(names, last, addresses, numbers, emails); break;
+            case 6: editContacts(names, last, addresses, numbers, emails); break;
             default: System.out.println("invalid input.");
         }
     }
     
     
     
-    public void askContactsInfo(ArrayList<String> names, ArrayList<String> addresses, ArrayList<Long> numbers, ArrayList<String> emails) {
+    public void askContactsInfo(ArrayList<String> names, ArrayList<String> last,ArrayList<String> addresses, ArrayList<String> numbers, ArrayList<String> emails) {
         Scanner input = new Scanner(System.in);
         System.out.print("enter name: ");
         String name = input.nextLine();
+        System.out.print("enter last name: ");
+        String lastname = input.nextLine();
         System.out.print("enter address: ");
         String address = input.nextLine();
-        long number = 0;
+        String number = "0";
         do {
             System.out.print("enter number: ");
-            number = input.nextLong();
-        } while (number <= 0);
+            number = input.nextLine();
+            if (number.length() == 1)  System.out.println("try again");
+        } while (number.length() == 1);
         System.out.print("enter email: ");
         String email = input.nextLine();
         input.nextLine();
         
-        names.add(name);
-        addresses.add(address);
-        numbers.add(number);
-        emails.add(email);
-        getQuestionsAndOptions(names, addresses, numbers, emails); 
+        if (names.contains(name)) {
+            System.out.println("name already exists");
+        } else {
+            names.add(name);
+            last.add(lastname);
+            addresses.add(address);
+            numbers.add(number);
+            emails.add(email);
+        }
+        getQuestionsAndOptions(names, last, addresses, numbers, emails); 
     }
     
     
     
-    public void findContact(ArrayList<String> names, ArrayList<String> addresses, ArrayList<Long> numbers, ArrayList<String> emails) {
+    public void findContact(ArrayList<String> names, ArrayList<String> last, ArrayList<String> addresses, ArrayList<String> numbers, ArrayList<String> emails) {
         Scanner input = new Scanner(System.in);
         System.out.print("enter number to find: ");
-        long number = input.nextLong();
-        if (!(number.isEmpty()) && numbers.contains(number)) {
+        String number = input.nextLine();
+        if (!(numbers.isEmpty()) && numbers.contains(number)) {
             int index = numbers.indexOf(number);
-            System.out.println();
+            System.out.println("name: " + names.get(index) + "\nLastname: " + last.get(index) + "\naddress: " + addresses.get(index) + "\nnumber: " + numbers.get(index) + "\nemail: " + emails.get(index));
+            getQuestionsAndOptions(names, last, addresses, numbers, emails); 
+        }
+        else {
+            System.out.println("name does not exist, try again.");
+            findContact(names, last, addresses, numbers, emails);
+        }
+    }
+    
+    
+    public void editContacts(ArrayList<String> names, ArrayList<String> last, ArrayList<String> addresses, ArrayList<String> numbers, ArrayList<String> emails) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter name of contact to edit: ");
+        String name = input.nextLine();
+        if (!(names.isEmpty()) && names.contains(name)) {
+            int index = names.indexOf(name);
+            System.out.print("change name: ");
+            String change = input.nextLine();
+            names.set(index, change);
+            System.out.print("change last name: ");
+            change = input.nextLine();
+            last.set(index, change);
+            System.out.print("change address: ");
+            change = input.nextLine();
+            addresses.set(index, change);
+            System.out.print("change number: ");
+            change = input.nextLine();
+            numbers.set(index, change);
+            System.out.print("change email: ");
+            change = input.nextLine();
+            emails.set(index, change);
+            
+            getQuestionsAndOptions(names, last, addresses, numbers, emails); 
+        }
+        else {
+            System.out.println("name does not exist, try again.");
+            editContacts(names, last, addresses, numbers, emails);
+        }
+    }
+    
+    
+    
+    public void findContactLastName(ArrayList<String> names, ArrayList<String> last, ArrayList<String> addresses, ArrayList<String> numbers, ArrayList<String> emails) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter last name to find: ");
+        String name = input.nextLine();
+        if (!(last.isEmpty()) && last.contains(name)) {
+            int index = last.indexOf(name);
+            System.out.println("name: " + names.get(index) + "\nlastname: " + last.get(index) + "\naddress: " + addresses.get(index) + "\nnumber: " + numbers.get(index) + "\nemail: " + emails.get(index));
+            getQuestionsAndOptions(names, last, addresses, numbers, emails); 
+        }
+        else {
+            System.out.println("name does not exist, try again.");
+            findContactLastName(names, last, addresses, numbers, emails);
+        }
+    }
+    
+    
+    public void findContactFirstName(ArrayList<String> names, ArrayList<String> last, ArrayList<String> addresses, ArrayList<String> numbers, ArrayList<String> emails) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter name to find: ");
+        String name = input.nextLine();
+        if (!(names.isEmpty()) && names.contains(name)) {
+            int index = names.indexOf(name);
+            System.out.println("name: " + names.get(index) + "\nlastname: " + last.get(index) + "\naddress: " + addresses.get(index) + "\nnumber: " + numbers.get(index) + "\nemail: " + emails.get(index));
+            getQuestionsAndOptions(names, last, addresses, numbers, emails); 
+        }
+        else {
+            System.out.println("name does not exist, try again.");
+            findContactFirstName(names, last, addresses, numbers, emails);
         }
     }
 
     
     
-    public void removeContact(ArrayList<String> names, ArrayList<String> addresses, ArrayList<Long> numbers, ArrayList<String> emails) {
+    public void removeContact(ArrayList<String> names, ArrayList<String> last, ArrayList<String> addresses, ArrayList<String> numbers, ArrayList<String> emails) {
         Scanner input = new Scanner(System.in);
         System.out.print("enter the name to be removed: ");
         String name = input.nextLine();
         if (!(names.isEmpty()) && names.contains(name)) {
             int index = names.indexOf(name);
             names.remove(index);
+            last.remove(index);
             addresses.remove(index);
             numbers.remove(index);
             emails.remove(index);
             System.out.print("contact have been deleted");
+            getQuestionsAndOptions(names, last, addresses, numbers, emails); 
         } 
         else {
             System.out.println("name does not exist, try again.");
-            removeContact(names, addresses, numbers, emails);
+            removeContact(names, last, addresses, numbers, emails);
         }
     }
 }
