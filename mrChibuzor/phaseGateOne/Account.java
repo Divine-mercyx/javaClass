56994bd7c713f17213fe0fe1f4e4810b2689825e
Divine Obinali <divinewilson766@gmail.com>
2024-12-05 14:25:21 +0100

    updated atmMachine

--- mrChibuzor/phaseGateOne/Account.java
+++ mrChibuzor/phaseGateOne/Account.java
@@ -4,42 +4,49 @@ public class Account {
     private String lastName;
     private double balance;
     private String pin;
+    private String accountNumber;
     
-    public Account(String firstName, String lastName, double balance, String pin) {
+    public Account(String firstName, String lastName,  String pin) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.pin = pin;
+        this.balance = 0.0;
+        this.accountNumber = generateAccountNumber();
         
-        if (balance > 0.0) {
-            this.balance = balance;
-        }
     }
     
     
     public double balance() {
-        return balance;
+        return this.balance;
     }
     
     public void deposit(double amount) {
         if (amount > 0.0) {
             this.balance += amount;
-            System.out.println("your account has been credited");
         }
     }
     
-    public void withdraw(double amount) {
-        if (amount > 0.0 && amount < balance) {
-            this.balance -= amount;
-            System.out.println("your account has been debited");
+    public void withdraw(double amount, String pin) {
+        if (getPin().equals(pin)) {
+            if (amount > 0.0 && amount < balance) {
+                this.balance -= amount;
+                System.out.println("$" + amount + " has been debited from your account");
+            } else {
+                System.out.println("insufficient funds");
+            }
         } else {
-            System.out.println("insufficient funds");
+            System.out.println("incorrect pin");
         }
     }
     
-    public void transfer(Account person, double amount) {
-        if (amount > 0.0) {
-            person.deposit(amount);
-            this.balance -= amount;
+    public void transfer(Account recipient, Account person, double amount, String pin) {
+        if (getPin().equals(pin)) {
+            if (amount > 0.0 && amount < person.balance) {
+                recipient.deposit(amount);
+                person.balance -= amount;
+            }
+        } else {
+                System.out.println("insufficient funds");
         }
     }
     
@@ -49,12 +56,34 @@ public class Account {
     }
     
     public String getName() {
-        return firstName;
+        return this.firstName + "  " + this.lastName;
+    }
+    
+    
+    public String getFirstName() {
+        return this.firstName;
     }
     
     
     public String getPin() {
-        return pin;
+        return this.pin;
+    }
+    
+    public String toString(){
+        return "Name: " + getName() + "\nAccount number: " + accountNumber + "\nBalance : " + balance + " \npin : " +  pin + "\n";
+    }
+    
+    public String generateAccountNumber() {
+        String accountNumber = "";
+        for (int index = 0; index <= 11; index++) {
+            accountNumber += (int) (Math.random() * 10);
+            if (accountNumber.length() == 11) break;
+        }
+        return accountNumber;
+    }
+    
+    public String getAccountNumber() {
+        return this.accountNumber;
     }
     
     
--- mrChibuzor/phaseGateOne/AtmMachine.java
+++ mrChibuzor/phaseGateOne/AtmMachine.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class AtmMachine {
-    static Scanner input = new Scanner(System.in);
+    static Scanner strings = new Scanner(System.in);
+    static Scanner integers = new Scanner(System.in);
     static ArrayList<Account> accounts = new ArrayList<>();
 
     
     public static void main(String[] args) {
-        
         displayHomePage();
         
     }
@@ -20,79 +20,100 @@ public class AtmMachine {
         2. login
         3. exit
         """);
-        int option = input.nextInt();
+        int option = integers.nextInt();
        switch (option) {
-          case 1: /*createAccount();*/ break;
+          case 1: createAccount(); break;
           case 2: login(); break;
           case 3: break;
           default: System.out.println("invalid input");
        }
     }
     
-    public static Account login() {
-        Scanner scanner = new Scanner(System.in);
+    public static void login() {
         System.out.println("LOGIN\n");
         System.out.print("enter name: ");
-        String name = scanner.nextLine();
+        String name = strings.nextLine();
         System.out.print("enter password: ");
-        String pin = scanner.nextLine();
-        Account empty = new Account(null, null, 0, null);
+        String pin = strings.nextLine();
         if (accounts.size() != 0) {
             for (Account account : accounts) {
-                if (account.getName().equalsIgnoreCase(name) && account.getPin().equals(pin)) {
+                if (account.getFirstName().equalsIgnoreCase(name) && account.getPin().equals(pin)) {
                      displayMenu(account);
                 }
-                else {
-                    System.out.println("login unsucessful");
-                    displayHomePage();
-                }
             }
+            displayHomePage();
         }
-        return empty;
     }
     
     
     public static void displayMenu(Account person) {
         System.out.println("""
-        1. close account
+        \n1. close account
         2. deposit money
         3. withdraw money
         4. check account balance
         5. transfer
         6. change pin
+        7. logout
         """);
-        int option = input.nextInt();
+        int option = integers.nextInt();
         switch (option) {
             case 1: closeAccount(person); break;
             case 2: depositMoney(person); break;
             case 3: withdraw(person); break;
             case 4: getBalance(person); break;
-            case 5: transfer(person); break;
+            case 5: transfers(person); break;
             case 6: break;
+            case 7: logout(); break;
             default: System.out.println("invalid input");
         }
     } 
     
     
-    /*public static void createAccount() {
+    public static void logout() {
+        displayHomePage();
+    }
+    
+    
+    public static void createAccount() {
             System.out.println("SIGNUP\n");
             System.out.print("enter you first name: ");
-            String firstName = input.nextLine();
-            input.nextLine();
+            String firstName = strings.nextLine();
             System.out.print("enter your last name: ");
-            String lastName = input.nextLine();
+            String lastName = strings.nextLine();
             System.out.print("create your pin: ");
-            String pin = input.nextLine();
-            accounts.add(new Account(firstName, lastName, 0.0, pin));
-            displayHomePage();
+            String pin = strings.nextLine();
+            Account account = new Account(firstName, lastName, pin);
+            System.out.print(account);
+            accounts.add(account);
+            login();
             
-    }*/
+    }
     
     
-    public static void transfer(Account person) {
+    public static void transfers(Account person) {
         System.out.print("choose the account number you want to transfer: ");
-        int accountNumber = input.nextInt();
+        String accountNumber = strings.nextLine();
+        getAccountByNumber(accountNumber, person);
+        displayMenu(person);
+    }
+    
+    public static void getAccountByNumber(String number, Account person) {
         
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(number)) {
+                displayDetails(account, person);
+            }
+        }
+    }
+    
+    public static void displayDetails(Account recipient, Account person) {
+        System.out.println(recipient);
+        System.out.print("how much do you want to transfer to " + recipient.getName() + ": ");
+        double amount = integers.nextInt();
+        System.out.print("enter your pin: ");
+        String pin = strings.nextLine();
+        person.transfer(recipient, person, amount, pin);
     }
     
     
@@ -112,7 +133,7 @@ public class AtmMachine {
     
     public static void depositMoney(Account person) {
         System.out.print("\nhow much do you want to deposit: ");
-        double amount = input.nextDouble();
+        double amount = integers.nextDouble();
         person.deposit(amount);
         System.out.println("account balance: " + person.balance());
         displayMenu(person);
@@ -121,8 +142,10 @@ public class AtmMachine {
     
     public static void withdraw(Account person) {
         System.out.print("\nhow much you want to withdraw: ");
-        double amount = input.nextDouble();
-        person.withdraw(amount);
+        double amount = integers.nextDouble();
+        System.out.print("enter you pin: ");
+        String pin = strings.nextLine();
+        person.withdraw(amount, pin);
         System.out.println("account balance: " + person.balance());
         displayMenu(person);
     }
