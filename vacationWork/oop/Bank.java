93bc6009e59396e0d99bc1f4ca7aad3014a78250
Divine Obinali <divinewilson766@gmail.com>
2024-12-17 18:25:54 +0100

    bank update

--- vacationWork/oop/Bank.java
+++ vacationWork/oop/Bank.java
@@ -4,10 +4,35 @@ import java.util.Scanner;
 public class Bank {
 
     private ArrayList<Account> accounts;
+    private String bankName;
+    private String location;
     
-    public Bank() {
+    public Bank(String name, String location) {
+        setBankName(name);
+        setLocation(location);
         accounts = new ArrayList<>();
     }
+    
+    
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    
+    public String getLocation() {
+        return this.location;
+    }
+    
+    
+    public void setBankName(String name) {
+        bankName = name;
+    }
+    
+    
+    public String getBankName() {
+        return bankName;
+    }
+    
     
     public void welcomePage() {
         Scanner input = new Scanner(System.in);
@@ -34,6 +59,7 @@ public class Bank {
         System.out.print("create your pin: ");
         String pin = input.nextLine();
         accountCreation(fullName, number, pin);
+        
         login();
     }
     
@@ -41,6 +67,7 @@ public class Bank {
     public void accountCreation(String fullName, String number, String pin) {
         Account account = new Account(fullName, number, pin);
         accounts.add(account);
+        System.out.println(account);
     }
     
     
@@ -50,15 +77,20 @@ public class Bank {
         String number = input.nextLine();
         System.out.print("enter pin: ");
         String pin = input.nextLine();
+        boolean found = false;
         
-        if (accounts.size() != 0) {
+        if (!accounts.isEmpty()) {
             for (Account account : accounts) {
-                if (account.getNumber().equals(number) && account.getPin().equals(pin)) {
+                if (account.getPin().equals(pin) && account.getNumber().equals(number)) {
+                    found = true;
                     displayAccountPage(account);
-                } else {
-                    System.out.println("incorrect number or pin");
+                    break;
                 }
             }
+             if (!found) {
+                    System.out.println("incorrect number or pin");
+                    welcomePage();
+                }
         } else {
             System.out.println("no accounts");
         }
@@ -66,15 +98,93 @@ public class Bank {
     
     
     public void displayAccountPage(Account account) {
+        Scanner input = new Scanner(System.in);
         System.out.println("""
         Welcome to the homepage
+        1. close account
+        2. deposit money
+        3. withdraw money
+        4. check account balance
+        5. transfer
+        6. change pin
+        7. logout
         """);
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1 -> closeAccount(account);
+            case 2 -> deposit(account);
+            case 3 -> withdraw(account);
+            case 4 -> checkBalance(account);
+            case 5 -> transfer(account);
+            case 7 -> logout();
+        }
+    }
+    
+    
+    public void logout() {
         welcomePage();
+    }
+    
+    
+    public void transfer(Account account) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("choose the account number you want to transfer: ");
+        String accountNumber = input.nextLine();
+        Account recipient = getAccount(accountNumber);
+        if (recipient != null) {
+            System.out.print("enter amount: ");
+            double amount = input.nextDouble();
+            System.out.println();
+            account.transfer(account, recipient, amount);
+            displayAccountPage(account);
+        } else {
+            System.out.println("account does not exist");
+            displayAccountPage(account);
+        }
+    }
+    
+    
+    public Account getAccount(String number) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(number)) {
+                return account;
+            }
+        }
+        return null;
+    }
+    
+    
+    public void checkBalance(Account account) {
+        System.out.println(account.getBalance());
+        displayAccountPage(account);
+    }
+    
+    
+    public void withdraw(Account account) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter amount: ");
+        double amount = input.nextDouble();
+        account.withdraw(amount);
+        displayAccountPage(account);
+    }
+    
+    
+    public void deposit(Account account) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter amount: ");
+        double amount = input.nextDouble();
+        account.deposit(amount);
+        displayAccountPage(account);
+    }
+    
+    
+    public void closeAccount(Account account) {
+        accounts.remove(account);
     }
 
 
     public static void main(String... args) {
-        Bank bank = new Bank();
+        Bank bank = new Bank("first bank", "lagos");
         bank.welcomePage();
     }
 }
